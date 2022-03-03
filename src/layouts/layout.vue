d634def2c71955cae3f9aaaa2b55f39b7f53099b
Dev.DY <kdydesign@gmail.com>
2022-03-03 17:40:15 +0900

    Create layout.vue

---
+++ src/layouts/layout.vue
@@ -0,0 +1,20 @@
+<!--
+  untitle
+
+  @since 2022-03-03
+  @author kdydesign.kim
+-->
+
+<template>
+  <div class="full-width row justify-center">
+    <div style="width: 1000px">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'layout'
+}
+</script>
