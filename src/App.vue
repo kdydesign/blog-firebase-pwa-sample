3fe7af266efd52673b1c07e5b08a3a0dfb9c5498
Dev.DY <kdydesign@gmail.com>
2022-03-03 17:41:04 +0900

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <layout/>
+  </div>
+</template>
+
+<script>
+import Layout from '@/layouts/layout'
+export default {
+  name: 'App',
+  components: {
+    Layout
+  }
+}
+</script>
+
+<style>
+</style>
