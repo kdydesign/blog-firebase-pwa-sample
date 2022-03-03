525203dbfa19b1e0ea80c5d04698c001d481e945
Dev.DY <kdydesign@gmail.com>
2022-03-03 17:39:54 +0900

    Create todo-list.vue

---
+++ src/components/todo-list.vue
@@ -0,0 +1,31 @@
+<!--
+  untitle
+
+  @since 2022-03-03
+  @author kdydesign.kim
+-->
+
+<template>
+  <q-card class="q-ma-sm"
+          @click="test">
+    <img src="https://cdn.quasar.dev/img/mountains.jpg">
+
+    <q-card-section>
+      <div class="text-h6">Card</div>
+      <div class="text-subtitle2">by John Doe</div>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+export default {
+  name: 'todo-list',
+  methods: {
+    test () {
+      this.$router.push('/add-todo')
+      console.log('22')
+    }
+  }
+}
+</script>
+
