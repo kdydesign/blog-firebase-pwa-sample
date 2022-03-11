335649e98c279679174337bc6878f2a95126cd2f
kdydesign <kdydesign@gmail.com>
2022-03-11 16:51:57 +0900

    chore: init project

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -2,9 +2,6 @@ import Vue from 'vue'
 
 import Firebase from 'firebase/app'
 
-import 'firebase/firebase-database'
-import 'firebase/firebase-storage'
-
 import FirebaseConfig from '../../firebase.config'
 
 Firebase.initializeApp(FirebaseConfig)
