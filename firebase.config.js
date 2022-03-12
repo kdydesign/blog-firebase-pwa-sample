1f07b4516580291d16d21dec34e6c4c1ba0e95b7
kdydesign <kdydesign@gmail.com>
2022-03-12 22:14:03 +0900

    chore: init project

--- firebase.config.js
+++ firebase.config.js
@@ -1,6 +1,7 @@
 export default {
   apiKey: 'AIzaSyA_DHLy4Y-kO0M2-A63z8Ytv1cgdJmpzyU',
   authDomain: 'todo-tutorial-4ccd2.firebaseapp.com',
+  databaseURL: 'https://todo-tutorial-4ccd2-default-rtdb.asia-southeast1.firebasedatabase.app',
   projectId: 'todo-tutorial-4ccd2',
   storageBucket: 'todo-tutorial-4ccd2.appspot.com',
   messagingSenderId: '211277786530',
