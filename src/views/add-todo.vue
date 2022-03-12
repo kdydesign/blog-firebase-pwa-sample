f24b884a48af1fb40db966c18a0f2eb3a4e6f830
kdydesign <kdydesign@gmail.com>
2022-03-12 23:03:19 +0900

    feat: remove todo list item

--- src/views/add-todo.vue
+++ src/views/add-todo.vue
@@ -9,22 +9,42 @@
   <div class="q-pa-xl">
     <div class="row justify-center q-gutter-md">
       <q-input
-          v-model="name"
-          outlined
-          label="Todo Name"
-          class="fit"
+        v-model="name"
+        outlined
+        label="Todo Name"
+        class="fit"
+        @keydown.enter="addTodo"
       >
-        <template v-slot:append>
-          <q-btn round dense flat icon="add"
-                 @click="addTodo"/>
+        <template #append>
+          <q-btn
+            round
+            dense
+            flat
+            icon="add"
+            @click="addTodo"
+          />
         </template>
       </q-input>
 
       <div class="col fit">
-        <div v-for="(c, i) in todoList"
-             :key="i"
-             class="row todo-item">
-          <q-checkbox v-model="c.check" :label="c.label" style="width:100%"/>
+        <div
+          v-for="(c, i) in todoList"
+          :key="i"
+          class="row todo-item"
+        >
+          <q-checkbox
+            v-model="c.check"
+            :label="c.label"
+            style="width:96%"
+          />
+
+          <q-btn
+            flat
+            dense
+            color="grey-7"
+            icon="delete"
+            @click="removeTodo(i)"
+          />
         </div>
       </div>
     </div>
@@ -33,7 +53,7 @@
 
 <script>
 export default {
-  name: 'add-todo',
+  name: 'AddTodo',
   data () {
     return {
       name: void 0,
@@ -43,16 +63,39 @@ export default {
   },
   methods: {
     addTodo () {
+      if (this.name === void 0) {
+        this.$q.notify({
+          message: '입력되지않았습니다.',
+          color: 'red',
+          timeout: 2000
+        })
+
+        return
+      }
+
       this.todoList.push({
         check: false,
         label: this.name
       })
+
+      this.name = void 0
+    },
+    removeTodo (idx) {
+      this.todoList.splice(idx, 1)
     }
   }
 }
 </script>
 
 <style lang="sass" scoped>
+.todo-item
+  display: flex
+  flex-direction: row
+  flex-wrap: nowrap
+  &:hover
+    transition: 0.5s
+    background: rgba(245, 245, 245, 0.68)
+
 .todo-item ::v-deep
   .q-checkbox__label
     text-overflow: ellipsis
